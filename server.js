34383ee84d92bd6a07caf9717af2ad3adea05548
scollins33 <sean_collins@gmx.com>
2017-11-17 22:13:53 -0500

    PORT env var not available

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const bgrCtrl = require('./controllers/burger_controller.js');
 
-const PORT = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 // create the application
 let app = express();
